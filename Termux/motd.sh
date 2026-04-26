330cf6cd86a26867a9fd628f1254ec324659d357
Abid Hasan Sojib <132114353+abidhasansojib@users.noreply.github.com>
2026-04-26 14:38:18 +0600

    Update welcome message in motd.sh
    Removed package management instructions from the welcome message.

--- Termux/motd.sh
+++ Termux/motd.sh
@@ -15,13 +15,4 @@ if [ -n "${T_APP_VERSION}" ] || [ -n "${T_APK_RELEASE}" ]; then
 fi
 # Welcome message
 echo -e "\e[0;32m$(toilet -t -f smslant -F crop Termux)\033[0m
-${T_APP_VERSION} ${T_APK_RELEASE}${nl}
-\e[0;32mWorking with packages\033[0m:
-  Search:  pkg search <query>
-  Install: pkg install <package>
-  Update:  pkg update
-\e[0;32mSubscribing to additional repos\033[0m:
-  Root:    pkg install root-repo
-  X11:     pkg install x11-repo
-\e[0;32mFor fixing any repository issues\033[0m:
-  Try \e[4mtermux-change-repo\e[0m command\n"
+${T_APP_VERSION} ${T_APK_RELEASE}${nl}"
